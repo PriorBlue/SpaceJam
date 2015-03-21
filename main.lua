d8839fe16241e6c266877944ca51a59b18eb2f56
Marcus Ihde <marcusplate@googlemail.com>
2015-03-21 18:42:42 +0100

    Add simple ship system.

--- main.lua
+++ main.lua
@@ -1,23 +1,24 @@
 local event = require("src/events")
+local ships = require("src/spaceship")
 
 function love.load()
-
+	ships.init(event)
+	-- TODO: remove dummies
+	event.push("CreateShip", {id = 1, x = 100, y = 100})
+	event.push("CreateShip", {id = 2, x = 200, y = 200})
+	event.push("CreateShip", {id = 3, x = 400, y = 300})
 end
 
 function love.update(dt)
-	event.push("ShootFire", {x = 16, y = 32})
-
-	bullets = event.pull("ShootFire")
+	ships.updateInput()
+	ships.update(dt)
 	
-	for k, b in pairs(bullets) do
-		print(b.x)
-	end
-	
+	-- clear events
 	event.clearAll()
 end
 
 function love.draw()
-    love.graphics.print("Hello World!", 200, 200)
+	ships.draw()
 end
 
 function resetGame()
@@ -25,7 +26,7 @@ function resetGame()
 end
 
 function love.mousepressed(x, y, button)
-  
+
 end
 
 function love.mousereleased(x, y, button)
@@ -37,7 +38,7 @@ function love.keypressed(key)
 end
 
 function love.keyreleased(key)
-  if key == 'escape' then
-        love.event.quit()
-  end
+	if key == 'escape' then
+		love.event.quit()
+	end
 end
