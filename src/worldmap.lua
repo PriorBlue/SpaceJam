03849d1b42a316b3a9b5fb6fff4d4d46b9da7f05
Supersonic112 <supersonic112@live.de>
2015-03-21 17:02:28 +0100

    initial files for the world map

---
+++ src/worldmap.lua
@@ -0,0 +1,56 @@
+-- worldmap.lua
+-- the world map has different sectors to select and points that mark special locations
+local wpoint = require "src/wpoint"
+local wsection = require "src/wsection"
+local worldmap = {}
+
+worldmap.sectionSize = 32 -- pixel border size of the sections
+
+function worldmap.init(len, wid) -- can also be used for a reset
+	local w = {}
+	w.length = len or 10
+	w.width = wid or 10
+	
+	w.map = {}		-- this table stores all the sections of the map
+	w.points = {}		-- this table just stores points
+	-- fill map with empty sections
+	for i=0,w.width-1 do
+		w.map[i] = {}
+		for j=0,w.length-1 do
+			local gameMap = nil -- TODO replace nil with new game map
+			local visual = nil -- TODO replace nil with a random integer value, see 
+			w.map[i][j] = wsection.new(gameMap,visual)
+		end
+	end
+
+	-- takes coordinates for the world map and puts it into the table
+	function w.addPoint(x,y,details)
+		local wmpoint = wpoint.new(x,y,details)
+		table.insert(points,wmpoint)
+	end
+	
+	-- returns a world map point at the given coordinate or nil
+	function w.getPointAt(x,y)
+		for k,v in pairs(points) do
+			if (v.x == x) and (v.y == y) then
+				return v
+			end
+		end
+		return nil
+	end
+	
+	function w.draw()
+		for k,i in pairs(w.map) do
+			for l,j in pairs(i) do
+				w.map[k][l].draw(k*worldmap.sectionSize,l*worldmap.sectionSize)
+			end
+		end
+		for k,v in pairs(w.points) do
+			v.draw() -- maybe add coordinates
+		end
+	end
+	
+	return w
+end
+
+return worldmap
---
+++ src/wsection.lua
@@ -0,0 +1,29 @@
+-- wsection
+-- this is a square on the world map and has a game map attatched to it
+wsection = {}
+function wsection.new(gameMap, visuals)
+	local s = {}
+	s.visuals = visuals or 1 -- index for the texture of this world section
+	s.map = gameMap
+
+	function s.draw(posX,posY)
+		--print("drawing a wsection at "..posX..", "..posY)
+	end
+	
+	-- optional function to get the game map for this world map section
+	function s.getGameMap()
+		if not s.map then
+			print("no sector here yet, please add code to automatically create a new one")
+		else
+			return s.map
+		end
+	end
+	
+	-- optional function to change the visuals for this world map section
+	function s.setVisuals(newVis)
+		s.visuals = newVis
+	end
+	return s
+end
+
+return wsection
