e3e187f9384fdd688eb448496ee2377e936b5596
Supersonic112 <supersonic112@live.de>
2015-03-21 23:21:04 +0100

    added the world map to main.lua (currently disabled) included some code in worldmap.lua that allows network events to change the world map sectors and markers markers and sectors now have a working draw method

--- main.lua
+++ main.lua
@@ -1,13 +1,19 @@
 local event = require("src/events")
 local ships = require("src/spaceship")
 local network = require("src/network")
+local worldmap = require("src/worldmap")
+
 require("src/map")
 require("src/camera")
 
 function love.load()
+	renderWorldMap = false -- set this to true if you want to see the world map
 	map = createMap()
 	network.init(event)
 	ships.init(event)
+	if renderWorldMap then
+		world = worldmap.init()
+	end
 	-- TODO: remove dummies
 	event.push("CreateShip", {id = 1, x = 100, y = 100})
 	event.push("CreateShip", {id = 2, x = 200, y = 200})
@@ -39,6 +45,9 @@ end
 function love.draw()
 	map.draw()
 	ships.draw()
+	if renderWorldMap then 
+		world.draw()
+	end
 end
 
 function resetGame()
