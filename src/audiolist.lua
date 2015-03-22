13773c63ebe68e331ddbe861938bb4e201c027b0
Supersonic112 <supersonic112@live.de>
2015-03-22 20:02:39 +0100

    just correcting a small error

--- src/audiolist.lua
+++ src/audiolist.lua
@@ -4,7 +4,7 @@ local audiolist = {
 		{"big_shot","sfx/big_shot.wav",1.0},
 		{"comm_noise","sfx/comm_noise.wav",1.2},
 		{"explosion","sfx/explosion.wav",1.0},
-		{"laser_beam","sfx/laser_beam.wav",1.0},
+		{"laser_beam","sfx/beam.wav",1.0},
 		{"menu_select","sfx/menu_select.wav",1.0},
 		{"menu_cancel","sfx/menu_cancel.wav",1.0},
 	},
